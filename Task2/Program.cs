2aaa737aab5aa4ad401c70c0f39feef3627e9382
Alexander Ryzhov <aryzhov84@gmail.com>
2022-08-03 21:44:25 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ведите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA*numberB==numberA)
+{
+    Console.WriteLine("Нет");
+}
+else
+{
+    Console.WriteLine("Да"); 
+}
